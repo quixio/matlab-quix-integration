56199cb01807fdb0b98fb01dd034ccc4832b79b4
Javier Blanco <javier+quixers@quix.io>
2025-07-22 09:37:16 +0000

    Edited 'main.py'

--- matlab-wheel/main.py
+++ matlab-wheel/main.py
@@ -17,7 +17,6 @@ def matlab_processing(row: dict):
 
     # Call function here
     output = quixmatlab_client.rot(v, theta)
-    print(output)
 
     # Incorporating result to row
     row["x_new"] = output[0][0]
