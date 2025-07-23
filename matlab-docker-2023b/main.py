a502cd321299bb96a03ba69ec759674e0ba4f554
Javier Blanco <javier+quixers@quix.io>
2025-07-23 15:29:27 +0000

    Edited 'main.py'

--- matlab-docker-2023b/main.py
+++ matlab-docker-2023b/main.py
@@ -2,20 +2,6 @@ from quixstreams import Application
 import math
 import os
 
-"""
-import matlab.engine
-
-print("Starting matlab")
-
-eng = matlab.engine.start_matlab()
-
-print("Calculate square root")
-output = eng.sqrt(8.0)
-
-print("Result:")
-print (output)
-"""
-
 import matlab.engine
 print("Starting matlab")
 eng = matlab.engine.start_matlab()
